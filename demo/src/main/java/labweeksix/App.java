458ca1711ce255c04901ffcf2efcb3cb3c5f2e17
Natnael <natnaelebuy@gmail.com>
2022-03-07 19:06:44 -0800

    update

--- demo/src/main/java/labweeksix/App.java
+++ demo/src/main/java/labweeksix/App.java
@@ -1,13 +1,18 @@
 package labweeksix;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String namesString[] = { "Geeks", "for", "Geeks" };
+        System.out.println(Arrays.toString(namesString));
     }
+            
+    
+    
 }
---
+++ demo/src/main/java/labweeksix/Student.java
@@ -0,0 +1,11 @@
+package labweeksix;
+
+public class Student {
+
+    public Student(int i, String string) {
+    }
+
+    public void display() {
+    }
+
+}
