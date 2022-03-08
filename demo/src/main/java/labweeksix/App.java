1140821f743073ab7c243ffdf243b142d23be758
Natnael <natnaelebuy@gmail.com>
2022-03-07 19:23:37 -0800

    new update

--- demo/src/main/java/labweeksix/App.java
+++ demo/src/main/java/labweeksix/App.java
@@ -1,7 +1,5 @@
 package labweeksix;
 
-import java.util.Arrays;
-
 /**
  * Hello world!
  *
@@ -9,9 +7,28 @@ import java.util.Arrays;
 public class App {
     public static void main( String[] args )
     {
-        String namesString[] = { "Geeks", "for", "Geeks" };
-        System.out.println(Arrays.toString(namesString));
+        String nameString[] = new String[3];
+        nameString[0] = "Geeks";
+        nameString[1] = "for";
+        nameString[2] = "Geeks";
+        {
+            System.out.print(nameString[0] + " ");
+            System.out.println(nameString[1] + " ");
+            System.out.println(nameString[2] + " ");
+
+        }
     }
+    public static void name(String[] args) {
+        Integer[] anArray = new Integer[10];
+
+        for (int i = 0; i < anArray.length; i++) {
+            anArray[i] = new Integer(i);
+            System.out.println(anArray[i]);
+        }
+        
+    }
+        
+    
             
     
     
